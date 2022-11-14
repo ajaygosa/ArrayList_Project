37074b6abe9b6af811decfe522e13107ab55efa0
Ajay Gosavi <ajaygosavi001@gmail.com>
2022-11-14 13:30:49 +0530

    my first commit

---
+++ ArrayList_Project/src/ArrayList_Demo.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList_Demo {
+
+	public static void main(String[] args) {
+
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		 Iterator itr = al.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
+
+}
